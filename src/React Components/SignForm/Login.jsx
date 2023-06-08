ba1d20c5de1a6fd1b3fcece4065257c130f23531
unknown <souravkgit>
2023-06-08 19:39:15 +0530

    updated login functionality

--- src/React Components/SignForm/Login.jsx
+++ src/React Components/SignForm/Login.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import "./Login.css";
+import "./Register.css";
 import { useState, useEffect } from 'react';
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
@@ -17,8 +18,9 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 
-export default function UserInteraction({ type }) {
-    const [formtype, setFormtype] = useState(type)
+export default function UserInteraction({prop} ) {
+    const [formtype, setFormtype] = useState(prop)
+    // console.log(prop,formtype)
     const [person, setPerson] = useState('Mentor')
     const auth = getAuth();
     const [user, setUser] = useState(null);
@@ -26,6 +28,7 @@ export default function UserInteraction({ type }) {
     const loginManually = async (e) => {
         console.log(e);
     };
+
 
     const loginWithGoogle = async () => {
         try {
@@ -59,14 +62,20 @@ export default function UserInteraction({ type }) {
         setPerson(event.target.value); // Update the selected role when the dropdown value changes
     };
 
-
+    const handleFormChange = (event) =>{
+        console.log(event.target.children[0].name);
+        const name = event.target.children[0].name;
+        const strings = name.split("-")
+        setFormtype(strings[1]);
+        setPerson(strings[0]);
+    }
 
     return (
         <div className="user-interaction-page">
-            <div className="login-box">
+            {formtype==='login' && (            <div className="login-box">
                 <div className="select-person">
                     <label for="membership">Select Type:</label>
-                    <select name="persontype" id="persontype" onChange={handleRoleChange} defaultValue="Mentor">
+                    <select name="persontype" id="persontype" onChange={handleRoleChange} defaultValue={person}>
                         <option value="Mentor">Mentor</option>
                         <option value="Mentee">Mentee</option>
                     </select>
@@ -103,7 +112,9 @@ export default function UserInteraction({ type }) {
                         </form>
                         <div className="sign-up-text">
                             Don't Have Account?
-                            <span> Sign Up Here</span>
+                            <span onClick={handleFormChange}>
+                            <input name="Mentor-register" type="hidden" />
+                                  Sign Up Here</span>
                         </div>
                     </>
                 )}
@@ -139,11 +150,119 @@ export default function UserInteraction({ type }) {
                         </form>
                         <div className="sign-up-text">
                             Don't Have Account?
-                            <span> Sign Up Here</span>
+                            <span onClick={handleFormChange}>
+                            <input name="Mentee-register" type="hidden" />
+                                 Sign Up Here</span>
                         </div>
                     </>
                 )}
-            </div>
+            </div>)}
+            {formtype==='register' && (            
+            <div className="register-box">
+                <div className="select-person">
+                    <label for="membership">Select Type:</label>
+                    <select name="persontype" id="persontype" onChange={handleRoleChange} defaultValue={person}>
+                        <option value="Mentor">Mentor</option>
+                        <option value="Mentee">Mentee</option>
+                    </select>
+                </div>
+                {person === "Mentor" && (
+                    <>
+                        <h2>Mentor Signup Form</h2>
+                        <form>
+                            <div className="user-box">
+                                <input type="text" name="email" required="" />
+                                <label>Email</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="password" name="password" required="" />
+                                <label>Password</label>
+                            </div>
+                            <input name='mentor' type="hidden" />
+                            <div className="btns-container">
+                                <a onClick={(e)=>{loginManually(e)}}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    Signup
+                                </a>
+                                <a className='google-sign-up' onClick={loginWithGoogle}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    Signup with Google
+                                </a>
+                            </div>
+                        </form>
+                        <div className="sign-up-text">
+                            Already Have Account?
+                            <span onClick={handleFormChange}>
+                            <input name="Mentor-login" type="hidden" />
+                                  Login Here</span>
+                        </div>
+                    </>
+                )}
+                {person === "Mentee" && (
+                    <>
+                        <h2>Mentee Signup Form</h2>
+                        <form>
+                            <div className="user-box">
+                                <input type="text" name="email" required="" />
+                                <label>Email</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" name="name" required="" />
+                                <label>Name</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" name="number" required="" />
+                                <label>Phone Number</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="date" name="dob" required="" id='date-input'/>
+                                <label>Date of Birth</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" name="gender" required="" />
+                                <label>Gender</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" name="location" required="" />
+                                <label>Current Location</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="password" name="password" required="" />
+                                <label>Password</label>
+                            </div>
+                            <input name='mentee' type="hidden" />
+                            <div className="btns-container">
+                                <a onClick={(e)=>{loginManually(e)}}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    Signup
+                                </a>
+                                <a className='google-sign-up' onClick={loginWithGoogle}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    Signup with Google
+                                </a>
+                            </div>
+                        </form>
+                        <div className="sign-up-text">
+                            Already Have Account?
+                            <span onClick={handleFormChange}>
+                            <input name="Mentee-login" type="hidden" />
+                                 Login Here</span>
+                        </div>
+                    </>
+                )}
+            </div>)}
         </div>
     );
 }
