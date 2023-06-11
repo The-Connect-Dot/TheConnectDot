a29da4bc59a21339ab7cf84f6857aa4923905342
omsri-bompelli <98706146+omsri-bompelli@users.noreply.github.com>
2023-06-11 11:04:21 +0530

    changed order

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./Pages/Home/Home";
 import Mentor from "./Pages/Mentor/Mentor";
 import Mentee from "./Pages/Mentee/Mentee";
 import Navbar from "./React Components/Navbar/Navbar";
-import SignupPage from "./Pages/SignupPage/SignupPage";
 import Login from "./React Components/SignForm/Login";
-import Registerpage  from "./Pages/Register/Registerpage";
-
 
 function App() {
   return (
@@ -20,7 +17,6 @@ function App() {
           <Route path="/Mentee" element={<Mentee />} />
           <Route path="/signup" element={<Login prop="register" />} />
           <Route path="/login" element={<Login prop="login"/>} />
-          <Route path="/register" element={<Registerpage />} />
         </Routes>
       </div>
     </BrowserRouter>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Mainsection from "../React Components/Main/Main_Page_1";
-import Footer from "../React Components/Footer/Footer";
+import Mainsection from "./Main_Page_1";
+import Footer from "../../React Components/Footer/Footer"
 // import Contact from "../React Components/Contact/Contact";
 // import Sign from "../React Components/SignForm/Sign";
 // import Testimonial from "../React Components/Testimonials/Testimonial";
