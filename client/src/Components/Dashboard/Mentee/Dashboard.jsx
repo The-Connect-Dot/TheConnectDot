fe22ea13222b37b9029694b0c6a8ff0c1e33a213
OMSRI BOMPELLI <81167782+om-sri@users.noreply.github.com>
2023-06-13 00:14:40 +0530

    Update Dashboard.jsx

--- client/src/Components/Dashboard/Mentee/Dashboard.jsx
+++ client/src/Components/Dashboard/Mentee/Dashboard.jsx
@@ -17,7 +17,10 @@ function Dashboard({ page }) {
         <div class="container-fluid xyz">
           <div class="row">
             <div class="col-lg-12">
-              <h1>Dashboard</h1>
+{/*               <h1>Dashboard</h1> */}
+              <div>
+                <Dashboardpage/>
+              </div>
             </div>
           </div>
         </div>
